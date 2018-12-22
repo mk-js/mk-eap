3a23f58963b55adac8efc022de4bd91cefe20c24
李胜国 <lsg@rrtimes.com>
2018-12-22 13:29:19 +0800

    fasterxml 2.9.8

---
+++ pkg.bat
@@ -0,0 +1,6 @@
+del /q lib\*.jar
+copy src\eap-base\eap-base-interface\target\*.jar lib
+copy src\eap-base\eap-base-service\target\*.jar lib
+copy src\eap-common\target\*.jar lib
+copy src\eap-component\eap-component-interface\target\*.jar lib
+copy src\eap-component\eap-component-service\target\*.jar lib
