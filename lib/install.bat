4cfb7ed2b80872d6b97a3e086803575b7b937a08
lsg <lsg@rrtimes.com>
2018-12-22 12:31:27 +0800

    version 0.0.1

---
+++ lib/install.bat
@@ -0,0 +1,6 @@
+
+mvn install:install-file -Dfile=./eap-base-interface-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-base-interface -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-base-service-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-base-service -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-common-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-common -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-component-interface-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-component-interface -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-component-service-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-component-service -Dversion=0.0.1 -Dpackaging=jar 
---
+++ lib/install.sh
@@ -0,0 +1,6 @@
+
+mvn install:install-file -Dfile=./eap-base-interface-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-base-interface -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-base-service-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-base-service -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-service-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-service -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-component-interface-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-component-interface -Dversion=0.0.1 -Dpackaging=jar 
+mvn install:install-file -Dfile=./eap-component-service-0.0.1.jar -DgroupId=com.mk.eap -DartifactId=eap-component-service -Dversion=0.0.1 -Dpackaging=jar 
