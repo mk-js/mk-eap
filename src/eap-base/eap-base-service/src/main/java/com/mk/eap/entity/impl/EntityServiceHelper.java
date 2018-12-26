14baa5d8e18861a60736e80c2d2ff356b1fa03bc
李胜国 <lsg@rrtimes.com>
2018-12-26 21:07:39 +0800

    EntityServiceHelper.java

---
+++ src/eap-base/eap-base-service/src/main/java/com/mk/eap/entity/impl/EntityServiceHelper.java
@@ -0,0 +1,126 @@
+package com.mk.eap.entity.impl;
+
+import java.io.Serializable;
+import java.lang.reflect.Type;
+
+import com.mk.eap.common.domain.DTO;
+import com.mk.eap.common.domain.VO;
+import com.mk.eap.entity.dao.EntityMapper;
+import com.mk.eap.entity.itf.IEntityService;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtField;
+import javassist.CtNewConstructor;
+import javassist.NotFoundException;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.SignatureAttribute;
+import javassist.bytecode.annotation.Annotation;
+
+public class EntityServiceHelper {
+	@SuppressWarnings("rawtypes")
+	public static IEntityService getService(String tableName,Type vo){
+		
+		try {
+			CtClass dtoClz = getDtoClass(tableName, vo);
+			
+			CtClass voClz = getVoClass(tableName, vo);
+			
+			CtClass mapperClz = getMapperClass(tableName, voClz);
+			
+			IEntityService serviceImpl = getEntityServiceClass(tableName, dtoClz, voClz, mapperClz);
+	 
+			return serviceImpl;
+			
+		} catch (CannotCompileException e) { 
+			e.printStackTrace();
+		} catch (NotFoundException e) { 
+			e.printStackTrace(); 
+		} catch (ClassNotFoundException e) { 
+			e.printStackTrace();
+		} catch (InstantiationException e) { 
+			e.printStackTrace();
+		} catch (IllegalAccessException e) { 
+			e.printStackTrace();
+		} 
+		
+		return null;
+	}
+	
+	private static IEntityService getEntityServiceClass(String tableName, CtClass dtoClz, CtClass voClz,
+			CtClass mapperClz) throws CannotCompileException, NotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException { 
+		
+		String className = "com.mk.eap.entity.dym." + tableName + "ServiceImpl";
+        ClassPool pool = ClassPool.getDefault();
+        CtClass subClass = pool.get(className);
+        if(subClass == null){
+            subClass = pool.makeClass( className);  
+            subClass.setSuperclass(pool.get(EntityServiceImpl.class.getName()));  
+            
+    		String genTypeName = "EntityServiceImpl<" + dtoClz.getName() + "," + voClz.getName() + "," + mapperClz.getName() + ">";
+            subClass.setGenericSignature(new SignatureAttribute.TypeVariable(genTypeName).encode());
+
+            CtClass[] params = new CtClass[]{ };
+            CtConstructor ctor = CtNewConstructor.make( params, null, CtNewConstructor.PASS_PARAMS, null, null, subClass );
+            subClass.addConstructor(ctor); 
+        }
+        Object serviceImpl = subClass.toClass().newInstance();
+        
+		return (IEntityService) serviceImpl;
+	}
+
+	private static CtClass getDtoClass(String tableName, Type vo) throws CannotCompileException, NotFoundException{
+		String className = tableName + "Dto";
+        ClassPool pool = ClassPool.getDefault();
+        CtClass subClass = pool.makeClass("com.mk.eap.entity.dym." + className);  
+        subClass.setSuperclass(pool.get(DTO.class.getName()));  
+        subClass.addInterface(pool.get(Serializable.class.getName())); 
+        subClass.addField(CtField.make("private static final long serialVersionUID = 1L;", subClass)); 
+        subClass.addField(CtField.make("public Long id;", subClass)); 
+        subClass.addField(CtField.make("public String code;", subClass)); 
+        subClass.addField(CtField.make("public String name;", subClass)); 
+		
+		return subClass;
+	}
+
+	private static CtClass getVoClass(String tableName, Type vo) throws CannotCompileException, NotFoundException{
+		String className = tableName ;
+        ClassPool pool = ClassPool.getDefault();
+        CtClass subClass = pool.makeClass("com.mk.eap.entity.dym." + className);  
+        subClass.setSuperclass(pool.get(VO.class.getName())); 
+        subClass.addInterface(pool.get(Serializable.class.getName())); 
+        subClass.addField(CtField.make("private static final long serialVersionUID = 1L;", subClass));
+        CtField idField = CtField.make("public Long id;", subClass);
+        
+        ConstPool constpool = subClass.getClassFile().getConstPool();
+		AnnotationsAttribute fieldAttr = new AnnotationsAttribute(constpool , AnnotationsAttribute.visibleTag);
+        Annotation autowired = new Annotation("javax.persistence.Id",constpool);
+        fieldAttr.addAnnotation(autowired); 
+        idField.getFieldInfo().addAttribute(fieldAttr);
+        
+        subClass.addField(idField); 
+        subClass.addField(CtField.make("public String code;", subClass)); 
+        subClass.addField(CtField.make("public String name;", subClass)); 
+		
+		return subClass;
+	}
+	
+	private static CtClass getMapperClass(String tableName,CtClass voClz) throws CannotCompileException, NotFoundException{
+		String className = tableName + "Mapper";
+        ClassPool pool = ClassPool.getDefault();
+        CtClass subClass = pool.makeClass("com.mk.eap.entity.dym." + className);  
+        subClass.setSuperclass(pool.get(EntityMapper.class.getName()));  
+        
+		String genTypeName = "EntityMapper<" + voClz.getName() + ">";
+        subClass.setGenericSignature(new SignatureAttribute.TypeVariable(genTypeName).encode());
+
+        CtClass[] params = new CtClass[]{ };
+        CtConstructor ctor = CtNewConstructor.make( params, null, CtNewConstructor.PASS_PARAMS, null, null, subClass );
+        subClass.addConstructor(ctor);
+        
+		return subClass;
+	}
+}
